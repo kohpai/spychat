c0282d3a75338d3c53375022740f303466018d73
Kohpai <whcwhc78@gmail.com>
2022-10-30 13:21:38 +0100

    :recycle: use key generation function from basic_utils instead

--- test/crypto_test.dart
+++ test/crypto_test.dart
@@ -1,16 +1,10 @@
-import 'package:pointycastle/api.dart';
-import 'package:pointycastle/ecc/api.dart';
-import 'package:pointycastle/src/platform_check/platform_check.dart';
-import 'package:spychat/crypto.dart';
+import 'package:basic_utils/basic_utils.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('Generate ECC key pair', () {
-    final secureRandom = SecureRandom('Fortuna')
-      ..seed(
-          KeyParameter(Platform.instance.platformEntropySource().getBytes(32)));
-    final AsymmetricKeyPair<ECPublicKey, ECPrivateKey> keyPair =
-        generateECkeyPair(secureRandom, domainName: 'secp384r1');
-    expect(keyPair.privateKey.parameters?.domainName, equals('secp384r1'));
+    final keyPair = CryptoUtils.generateEcKeyPair(curve: 'secp384r1');
+    final privateKey = keyPair.privateKey as ECPrivateKey;
+    expect(privateKey.parameters?.domainName, equals('secp384r1'));
   });
 }
