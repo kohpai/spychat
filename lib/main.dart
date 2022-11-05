0dc0d13e316acecbd1ddd420d830f7767ead6155
Kohpai <whcwhc78@gmail.com>
2022-11-05 20:06:46 +0100

    :recycle: move getKeyPair() into KeyPemDisplay

--- lib/main.dart
+++ lib/main.dart
@@ -41,40 +41,41 @@ class MyApp extends StatelessWidget {
   }
 }
 
-Future<AsymmetricKeyPair> getKeyPair() async {
-  const storage = FlutterSecureStorage();
-
-  return Future.wait(
-      [storage.read(key: 'public_key'), storage.read(key: 'private_key')])
-      .then((pems) async {
-    if (pems.any((pem) => pem == null)) {
-      developer.log('Key(s) NOT found', name: 'me.kohpai.main');
-      final keyPair = CryptoUtils.generateEcKeyPair(curve: 'secp384r1');
-      await Future.wait([
-        storage.write(
-            key: 'public_key',
-            value: CryptoUtils.encodeEcPublicKeyToPem(
-                keyPair.publicKey as ECPublicKey)),
-        storage.write(
-            key: 'private_key',
-            value: CryptoUtils.encodeEcPrivateKeyToPem(
-                keyPair.privateKey as ECPrivateKey))
-      ]);
-      return keyPair;
-    }
-
-    developer.log('Keys FOUND', name: 'me.kohpai.main');
-    final publicKey = CryptoUtils.ecPublicKeyFromPem(pems[0]!);
-    final privateKey = CryptoUtils.ecPrivateKeyFromPem(pems[1]!);
-    return AsymmetricKeyPair(publicKey, privateKey);
-  });
-}
-
 class KeyPemDisplay extends StatelessWidget {
   late final Future<AsymmetricKeyPair> futureKeyPair;
 
   KeyPemDisplay({super.key}) {
     futureKeyPair = getKeyPair();
+  }
+
+  //TODO: make this into a singleton for accessing the key pair
+  Future<AsymmetricKeyPair> getKeyPair() async {
+    const storage = FlutterSecureStorage();
+
+    return Future.wait(
+        [storage.read(key: 'public_key'), storage.read(key: 'private_key')])
+        .then((pems) async {
+      if (pems.any((pem) => pem == null)) {
+        developer.log('Key(s) NOT found', name: 'me.kohpai.main');
+        final keyPair = CryptoUtils.generateEcKeyPair(curve: 'secp384r1');
+        await Future.wait([
+          storage.write(
+              key: 'public_key',
+              value: CryptoUtils.encodeEcPublicKeyToPem(
+                  keyPair.publicKey as ECPublicKey)),
+          storage.write(
+              key: 'private_key',
+              value: CryptoUtils.encodeEcPrivateKeyToPem(
+                  keyPair.privateKey as ECPrivateKey))
+        ]);
+        return keyPair;
+      }
+
+      developer.log('Keys FOUND', name: 'me.kohpai.main');
+      final publicKey = CryptoUtils.ecPublicKeyFromPem(pems[0]!);
+      final privateKey = CryptoUtils.ecPrivateKeyFromPem(pems[1]!);
+      return AsymmetricKeyPair(publicKey, privateKey);
+    });
   }
 
   @override
