539eb0363538515fa87902f8aa662b1fb1d66f69
Kohpai <whcwhc78@gmail.com>
2022-11-05 15:05:19 +0100

    :white_check_mark: remove generate key pair test

--- test/crypto_test.dart
+++ test/crypto_test.dart
@@ -1,12 +1,7 @@
 import 'package:basic_utils/basic_utils.dart';
-import 'package:test/test.dart';
 
-void main() {
-  test('Generate ECC key pair', () {
-    final keyPair = CryptoUtils.generateEcKeyPair(curve: 'secp384r1');
-    final privateKey = keyPair.privateKey as ECPrivateKey;
-    expect(privateKey.parameters?.domainName, equals('secp384r1'));
-  });
+void main() async {
+  // printEcKeyPair();
 }
 
 void printEcKeyPair() {
