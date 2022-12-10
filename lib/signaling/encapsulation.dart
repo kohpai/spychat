4881732fc5ee5703d93b95d081c0b8dff867fa13
Kohpai <whcwhc78@gmail.com>
2022-12-10 16:23:37 +0100

    :bug: fix encapsulating signaling packet

--- lib/signaling/encapsulation.dart
+++ lib/signaling/encapsulation.dart
@@ -5,14 +5,14 @@ import 'package:flutter/foundation.dart';
 import 'package:spychat/signaling/kay_pair_provider.dart';
 import 'package:spychat/signaling/packet.dart';
 
-Future<String> encapsulatePacket([String? data]) async {
+Future<String> encapsulatePacket([String? dstPubKey, String data = ""]) async {
   final keyPair = await KeyPairProvider.getKeyPair();
 
   final pubKey = keyPair.first;
   final now = DateTime.now().toUtc();
-  final packet = data == null
+  final packet = dstPubKey == null
       ? Packet.connect(pubKey, now)
-      : Packet.signal(pubKey, data, now);
+      : Packet.signal(dstPubKey, data, now);
   final packetJson = jsonEncode(packet);
   final signature = CryptoUtils.ecSign(
       keyPair.last, Uint8List.fromList(utf8.encode(packetJson)), algorithmName: 'SHA-256/ECDSA');
